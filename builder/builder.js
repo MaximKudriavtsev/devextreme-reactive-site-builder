ede1280ddc5e7b7fad50a5df4190496b9c620b7a
Yuriy Orlov (DevExpress) <Orlov.Yuriy@corp.devexpress.com>
2018-04-28 11:28:10 +0300

    allow to build branches

--- builder/builder.js
+++ builder/builder.js
@@ -20,6 +20,40 @@ const appendToIndexFile = (string) => {
     'utf-8',
   );
 };
+const buildSite = async (repository, sha, name, title) => {
+  let meta = '';
+  try {
+    meta = JSON.parse(readFileSync(join(BUILT_SITE_FOLDER, name, META_FILE), 'utf-8'));
+  } catch(e) {}
+  if (meta === sha) {
+    appendToIndexFile(`<a href="${name}/">${title}</a><br />`);
+    return;
+  }
+
+  try {
+    removeSync(join(__dirname, REPO_FOLDER));
+    execSync(`git clone https://github.com/${repository}.git ${REPO_FOLDER}`, { stdio: 'ignore' });
+    execSync(`git checkout ${sha}`, { cwd: join(__dirname, REPO_FOLDER), stdio: 'ignore' });
+    
+    removeSync(join(BUILT_SITE_FOLDER, name));
+    execSync(`yarn --no-progress`, { cwd: join(__dirname, REPO_FOLDER), stdio: 'ignore' });
+    execSync(`yarn build:site`, { cwd: join(__dirname, REPO_FOLDER), stdio: 'ignore' });
+    execSync('bundle install', { cwd: join(__dirname, SITE_FOLDER) });
+    writeFileSync(join(__dirname, SITE_FOLDER, GENERATED_CONFIG_FILE), `baseurl: "/${name}"`);
+    execSync(
+      `bundle exec jekyll build --config _config.yml,${GENERATED_CONFIG_FILE} --source ${join(__dirname, SITE_FOLDER)} --destination ${join(BUILT_SITE_FOLDER, name)}`,
+      { cwd: join(__dirname, SITE_FOLDER), stdio: 'ignore' },
+    );
+    writeFileSync(
+      join(BUILT_SITE_FOLDER, name, META_FILE),
+      JSON.stringify(sha),
+      'utf-8',
+    );
+    appendToIndexFile(`<a href="${name}/">${title}</a><br />`);
+  } catch(e) {
+    appendToIndexFile(`<span>${title} [BUILD FAILED]</span><br />`);
+  }
+};
 
 const script = async () => {
   while(true) {
@@ -29,10 +63,19 @@ const script = async () => {
       hour: 'numeric', minute: 'numeric', second: 'numeric',
       timeZone: "Europe/Moscow",
     };
-    appendToIndexFile(`<b>Build time: ${new Intl.DateTimeFormat('en-US', formatterOptions).format(new Date())}</b><br/><br/>`);
+    appendToIndexFile(`<b>Build time: ${new Intl.DateTimeFormat('en-US', formatterOptions).format(new Date())}</b><br/>`);
 
-    appendToIndexFile(`<b>PRs:</b><br/>`);
-    console.log('Check for pr updates...');
+    const branches = JSON.parse(await request(`https://api.github.com/repos/${REPO}/branches`, {
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASS,
+        },
+        headers: {
+          'User-Agent': 'request',
+        },
+      }))
+      .filter(branch => branch.name !== 'gh-pages');
+
     const prs = JSON.parse(await request(`https://api.github.com/repos/${REPO}/pulls`, {
         auth: {
           user: process.env.USER,
@@ -43,50 +86,28 @@ const script = async () => {
         },
       }));
 
-    console.log(`Removing stale sites...`);
     readdirSync(BUILT_SITE_FOLDER).forEach(filename => {
-      if (!filename.startsWith('pr')) return;
-      const prNumber = JSON.parse(filename.replace('pr', ''));
-      if (prs.findIndex(pr => pr.number === prNumber) === -1) {
-        removeSync(join(BUILT_SITE_FOLDER, filename))
+      if (filename.startsWith('pr')) {
+        const prNumber = JSON.parse(filename.replace('pr', ''));
+        if (prs.findIndex(pr => pr.number === prNumber) === -1) {
+          removeSync(join(BUILT_SITE_FOLDER, filename))
+        }
+      }
+      if (filename.startsWith('branch')) {
+        const branchName = filename.replace('branch', '');
+        if (branches.findIndex(branch => branch.name === branchName) === -1) {
+          removeSync(join(BUILT_SITE_FOLDER, filename))
+        }
       }
     });
 
-    console.log(`Generating sites...`);
+    appendToIndexFile(`<br/><b>Branches:</b><br/>`);
+    branches.forEach(async branch => {
+      await buildSite(REPO, branch.commit.sha, `branch${branch.name}`, branch.name);
+    });
+    appendToIndexFile(`<br/><b>PRs:</b><br/>`);
     prs.forEach(async pr => {
-      let meta = '';
-      try {
-        meta = JSON.parse(readFileSync(join(BUILT_SITE_FOLDER, `pr${pr.number}`, META_FILE), 'utf-8'));
-      } catch(e) {}
-      if (meta === pr.head.sha) {
-        appendToIndexFile(`<a href="pr${pr.number}/">${pr.title}</a><br />`);
-        return;
-      }
-
-      console.log(`Building site for: pr${pr.number}...`);
-      try {
-        removeSync(join(__dirname, REPO_FOLDER));
-        execSync(`git clone https://github.com/${pr.head.repo.full_name}.git ${REPO_FOLDER}`, { stdio: 'ignore' });
-        execSync(`git checkout ${pr.head.sha}`, { cwd: join(__dirname, REPO_FOLDER), stdio: 'ignore' });
-        
-        removeSync(join(BUILT_SITE_FOLDER, `pr${pr.number}`));
-        execSync(`yarn --no-progress`, { cwd: join(__dirname, REPO_FOLDER), stdio: 'ignore' });
-        execSync(`yarn build:site`, { cwd: join(__dirname, REPO_FOLDER), stdio: 'ignore' });
-        execSync('bundle install', { cwd: join(__dirname, SITE_FOLDER) });
-        writeFileSync(join(__dirname, SITE_FOLDER, GENERATED_CONFIG_FILE), `baseurl: "/pr${pr.number}"`);
-        execSync(
-          `bundle exec jekyll build --config _config.yml,${GENERATED_CONFIG_FILE} --source ${join(__dirname, SITE_FOLDER)} --destination ${join(BUILT_SITE_FOLDER, `pr${pr.number}`)}`,
-          { cwd: join(__dirname, SITE_FOLDER), stdio: 'ignore' },
-        );
-        writeFileSync(
-          join(BUILT_SITE_FOLDER, `pr${pr.number}`, META_FILE),
-          JSON.stringify(pr.head.sha),
-          'utf-8',
-        );
-        appendToIndexFile(`<a href="pr${pr.number}/">${pr.title}</a><br />`);
-      } catch(e) {
-        appendToIndexFile(`<span>${pr.title} [BUILD FAILED]</span><br />`);
-      }
+      await buildSite(pr.head.repo.full_name, pr.head.sha, `pr${pr.number}`, pr.title);
     });
 
     await sleep(3 * 60 * 1000);
